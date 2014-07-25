c8f9ac337688995b6364250548dfd6c50e97c8a5
Eric Herot <eric.github@herot.com>
2014-07-25 12:04:58 -0400

    First rev

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of pam
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'pam'
+maintainer       'EverTrue, Inc.'
+maintainer_email 'eric.herot@evertrue.com'
+license          'Apache 2.0'
+description      'Installs/Configures pam'
+long_description 'Installs/Configures pam'
+version          '0.1.0'
+
