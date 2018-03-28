2bcff5ed7c4576d4d75b9b2e484f841fca89add5
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-03-28 15:10:28 -0400

    Add chef_version metadata

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures pam'
 long_description 'Installs/Configures pam'
 version          '1.0.4'
+chef_version     '>= 12.1'
 
 issues_url 'https://github.com/evertrue/pam-cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/evertrue/pam-cookbook' if respond_to?(:source_url)
