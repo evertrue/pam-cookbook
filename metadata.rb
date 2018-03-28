3b8bd8c7f9ddb45b3812dc4c86e8d77fa444d680
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-03-28 16:02:21 -0400

    FC069 & FC078: Fix license metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'pam'
 maintainer       'EverTrue, Inc.'
 maintainer_email 'devops@evertrue.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures pam'
 long_description 'Installs/Configures pam'
 version          '1.0.4'
